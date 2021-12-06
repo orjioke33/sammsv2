42351490893c1f0cd9de9dd11978bc11e64414b5
Ozi Orjioke <orjioke33@gmail.com>
2021-12-05 23:58:29 -0500

    patch: add filter to main
    - and nlms filter to samms_main

--- samms_main/samms_main.ino
+++ samms_main/samms_main.ino
@@ -1,9 +1,97 @@
+// Filter Headers
+#include <MP.h>
+#include <Audio.h>
+
+// Accelerometer Headers
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL343.h>
+
+// Filter Variables
+AudioClass *theAudio;
+// TO DO: Move these to a separate filter file
+const int mic_channel_num = 2;
+
+const int subcore = 1;
+
+struct Capture 
+{
+  void *buff;
+  int  sample;
+  int  chnum;
+};
+
+struct Result 
+{
+  void *buff;
+  int sample;
+};
+
 void setup() {
-  // put your setup code here, to run once:
+  // Filter Setup - TO DO Cleanup
+    int ret;
+
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.println("Init Audio Library");
+  theAudio = AudioClass::getInstance();
+  theAudio->begin();
+
+  Serial.println("Init Audio Recorder");
+  /* Select input device as AMIC */
+  //theAudio->setRecorderMode(AS_SETRECDR_STS_INPUTDEVICE_MIC, 210);
+  theAudio->setRecorderMode(AS_SETRECDR_STS_INPUTDEVICE_MIC);
+
+  /* Set PCM capture */
+  uint8_t channel;
+  switch (mic_channel_num) {
+  case 1: channel = AS_CHANNEL_MONO;   break;
+  case 2: channel = AS_CHANNEL_STEREO; break;
+  case 4: channel = AS_CHANNEL_4CH;    break;
+  }
+  theAudio->initRecorder(AS_CODECTYPE_PCM, "/mnt/sd0/BIN", AS_SAMPLINGRATE_48000, channel);
+
+  /* Launch SubCore */
+  ret = MP.begin(subcore);
+  if (ret < 0) {
+    printf("MP.begin error = %d\n", ret);
+  }
+
+  Serial.println("Rec start!");
+  theAudio->startRecorder();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Filter - TO Do Cleanup
+    int8_t sndid = 100; /* user-defined msgid */
+  int8_t rcvid = 8;
+  Capture  capture;
+
+  static const int32_t buffer_sample = 768 * mic_channel_num;
+  static const int32_t buffer_size = buffer_sample * sizeof(int16_t);
+  static char  buffer[buffer_size];
+  uint32_t read_size;
+
+  /* Read frames to record in buffer */
+  int err = theAudio->readFrames(buffer, buffer_size, &read_size);
+
+  if (err != AUDIOLIB_ECODE_OK && err != AUDIOLIB_ECODE_INSUFFICIENT_BUFFER_AREA) {
+    printf("Error err = %d\n", err);
+    sleep(1);
+    theAudio->stopRecorder();
+    exit(1);
+  }
+
+  if ((read_size != 0) && (read_size == buffer_size)) {
+    capture.buff   = buffer;
+    capture.sample = buffer_sample / mic_channel_num;
+    capture.chnum  = mic_channel_num;
+    MP.Send(sndid, &capture, subcore);
+    //Serial.println("Sending Data to Subcore1");
+  } else {
+    usleep(1);
+  }
 
 }
